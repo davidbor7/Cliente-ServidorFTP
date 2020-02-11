5f9dac84197feeed4b6aa2fc6d6b47452c01cba4
davidbor7 <borregodavid@hotmail.com>
2020-02-11 17:51:57 +0100

    .

--- Cliente-ServidorFTP/src/ClienteFTP/ClienteFTP.java
+++ Cliente-ServidorFTP/src/ClienteFTP/ClienteFTP.java
@@ -17,9 +17,8 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.io.IOException;
 import java.awt.Dimension;
-
-
 import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.ImageIcon;
@@ -50,6 +49,7 @@ public class ClienteFTP extends JFrame implements ListSelectionListener, MouseLi
 	private static int contador;
 	private static String acumulador_directorios[]= new String[5];
 	private JLabel laber_directorio;
+	private JButton crear_carpeta;
 	/**
 	 * Create the frame.
 	 */
@@ -79,9 +79,9 @@ public class ClienteFTP extends JFrame implements ListSelectionListener, MouseLi
 		scrollPane.setBounds(10, 47, 335, 384);
 		contentPane.add(scrollPane);
 
-		JButton btnNewButton = new JButton("Crear");
-		btnNewButton.setBounds(355, 88, 102, 23);
-		contentPane.add(btnNewButton);
+		crear_carpeta = new JButton("Crear");
+		crear_carpeta.setBounds(355, 88, 102, 23);
+		contentPane.add(crear_carpeta);
 
 		JButton btnEliminarCarpeta = new JButton("Eliminar");
 		btnEliminarCarpeta.setBounds(355, 122, 102, 23);
@@ -101,7 +101,7 @@ public class ClienteFTP extends JFrame implements ListSelectionListener, MouseLi
 		boton_adelantar.setIcon(new javax.swing.ImageIcon(ClienteFTP.class.getResource("/ClienteFTP/adelantar.png")));
 		contentPane.add(boton_adelantar);
 
-		
+
 		boton_refrescar = new JButton("");
 		boton_refrescar.setBounds(105, 9, 30, 30);
 		boton_refrescar.setIcon(new javax.swing.ImageIcon(ClienteFTP.class.getResource("/ClienteFTP/actualizar-removebg-preview.png")));
@@ -111,51 +111,51 @@ public class ClienteFTP extends JFrame implements ListSelectionListener, MouseLi
 		boton_home.setIcon(new javax.swing.ImageIcon(ClienteFTP.class.getResource("/ClienteFTP/home (1).png")));
 		boton_home.setBounds(145, 9, 30, 30);	
 		contentPane.add(boton_home);
-		
+
 		JLabel lblNewLabel = new JLabel("CARPETAS");
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setForeground(new Color(173, 255, 47));
 		lblNewLabel.setFont(new Font("Arial", Font.BOLD, 17));
 		lblNewLabel.setBounds(355, 36, 102, 23);
 		contentPane.add(lblNewLabel);
-		
+
 		JSeparator separator = new JSeparator();
 		separator.setBounds(355, 70, 102, 2);
 		contentPane.add(separator);
-		
+
 		JLabel lblFicheros = new JLabel("FICHEROS");
 		lblFicheros.setHorizontalAlignment(SwingConstants.CENTER);
 		lblFicheros.setForeground(new Color(173, 255, 47));
 		lblFicheros.setFont(new Font("Arial", Font.BOLD, 17));
 		lblFicheros.setBounds(355, 190, 102, 23);
 		contentPane.add(lblFicheros);
-		
+
 		JSeparator separator_1 = new JSeparator();
 		separator_1.setBounds(355, 224, 102, 2);
 		contentPane.add(separator_1);
-		
+
 		JButton button = new JButton("Renombrar");
 		button.setBounds(355, 306, 102, 23);
 		contentPane.add(button);
-		
+
 		JButton btnBajar = new JButton("Bajar");
 		btnBajar.setBounds(355, 272, 102, 23);
 		contentPane.add(btnBajar);
-		
+
 		JButton btnSubir = new JButton("Subir");
 		btnSubir.setBounds(355, 238, 102, 23);
 		contentPane.add(btnSubir);
-		
+
 		JButton btnBorrar = new JButton("Borrar");
 		btnBorrar.setBounds(355, 340, 102, 23);
 		contentPane.add(btnBorrar);
-		
+
 		laber_directorio = new JLabel("");
 		laber_directorio.setForeground(new Color(173, 255, 47));
 		laber_directorio.setFont(new Font("Arial", Font.BOLD | Font.ITALIC, 14));
 		laber_directorio.setBounds(91, 432, 368, 27);
 		contentPane.add(laber_directorio);
-		
+
 		JLabel lblDirectorio = new JLabel("Directorio:");
 		lblDirectorio.setForeground(new Color(173, 255, 47));
 		lblDirectorio.setBackground(SystemColor.textHighlight);
@@ -183,6 +183,7 @@ public class ClienteFTP extends JFrame implements ListSelectionListener, MouseLi
 
 
 		// --- AÑADIMOS LOS LISTENER---
+		crear_carpeta.addActionListener(this);
 		boton_home.addActionListener(this);
 		boton_volver.addActionListener(this);
 		boton_adelantar.addActionListener(this);
@@ -331,37 +332,79 @@ public class ClienteFTP extends JFrame implements ListSelectionListener, MouseLi
 	@Override
 	public void actionPerformed(ActionEvent ae) 
 	{
-		if (ae.getSource().equals(boton_volver)) 
-		{
-			try 
-			{			
-				if (contador > 0) 
-				{
-					contador--;
 
-					System.out.println(acumulador_directorios[contador]);
-					directorioActual= acumulador_directorios[contador];
-					laber_directorio.setText(directorioActual);
-					//Se establece el directorio de trabajo actual
-					cliente.changeWorkingDirectory(acumulador_directorios[contador]);
-					//Obteniendo ficheros y directorios del directorio actual
-					files = cliente.listFiles();
-					llenarLista(files);
-					//---------------------------------------------
+		if(ae.getSource().equals(crear_carpeta))
+		{
+			String nombreCarpeta = JOptionPane.showInputDialog(null, "Introduce el nombre de la carpeta","");
+			if (!(nombreCarpeta==null)) 
+			{
+				try 
+				{
+					System.out.println(directorioActual + "/"+ nombreCarpeta);
+
+					if (cliente.makeDirectory(directorioActual + "/"+ nombreCarpeta))
+					{
+						String m = nombreCarpeta.trim()+ " => Se ha creado correctamente ...";
+						JOptionPane.showMessageDialog(null, m);
+
+						//RECARGARMOS LA LISTA
+						try 
+						{
+							//Obteniendo ficheros y directorios del directorio actual
+							files = cliente.listFiles();
+							llenarLista(files);
+							//---------------------------------------------
+
+						} catch (Exception e) 
+						{
+							System.out.println(e.getMessage());
+						}
+
+					}else
+					{
+						JOptionPane.showMessageDialog(null, nombreCarpeta.trim() + " => No se ha podido crear ...");
+					}				
+				}
+				catch (IOException e1)
+				{
+					e1.printStackTrace();
 				}
 
-
-			} catch (Exception e) 
-			{
-				System.out.println(e.getMessage());
 			}
-		}else 
-		{
-			if (ae.getSource().equals(boton_adelantar)) 
+
+
+
+			if (ae.getSource().equals(boton_volver)) 
 			{
-				
 				try 
 				{			
+					if (contador > 0) 
+					{
+						contador--;
+
+						System.out.println(acumulador_directorios[contador]);
+						directorioActual= acumulador_directorios[contador];
+						laber_directorio.setText(directorioActual);
+						//Se establece el directorio de trabajo actual
+						cliente.changeWorkingDirectory(acumulador_directorios[contador]);
+						//Obteniendo ficheros y directorios del directorio actual
+						files = cliente.listFiles();
+						llenarLista(files);
+						//---------------------------------------------
+					}
+
+
+				} catch (Exception e) 
+				{
+					System.out.println(e.getMessage());
+				}
+			}else 
+			{
+				if (ae.getSource().equals(boton_adelantar)) 
+				{
+
+					try 
+					{			
 
 						if (acumulador_directorios[contador+1]!= null) 
 						{
@@ -377,52 +420,53 @@ public class ClienteFTP extends JFrame implements ListSelectionListener, MouseLi
 							//---------------------------------------------
 						}
 
-				} catch (Exception e) 
-				{
-					System.out.println(e.getMessage());
-				}
-				
-
-			}
-			else 
-			{
-				if (ae.getSource().equals(boton_refrescar)) 
-				{
-
-					System.out.println("Hola");
-					try 
-					{
-						//Obteniendo ficheros y directorios del directorio actual
-						files = cliente.listFiles();
-						llenarLista(files);
-						//---------------------------------------------
-
 					} catch (Exception e) 
 					{
 						System.out.println(e.getMessage());
 					}
+
+
 				}
 				else 
 				{
-					if (ae.getSource().equals(boton_home)) 
-					{	
+					if (ae.getSource().equals(boton_refrescar)) 
+					{
+
+						System.out.println("Hola");
 						try 
 						{
-							acumulador_directorios = new String[15];//REINICIAMOS
-							acumulador_directorios[0] = "/";
-							
-							directorioActual = directorioInicial; //EL DIRECTORIO ACTUAL PASA A SER LA CARPETA RAÍZ
-							laber_directorio.setText(directorioActual);
-							contador=0; //REINICIAMOS EL CONTADOR DE RUTAS					
-							cliente.changeWorkingDirectory(directorioInicial);//SE ESTABLECE EL DIRECTORIO ACTUAL DE TRABAJO
-							//OBTIENE LOS FICHEROS Y ARCHIVOS DEL DIRECTORIO
+							//Obteniendo ficheros y directorios del directorio actual
 							files = cliente.listFiles();
-							llenarLista(files);//ACTUALIZA LA LISTA
+							llenarLista(files);
 							//---------------------------------------------
+
 						} catch (Exception e) 
 						{
 							System.out.println(e.getMessage());
-						}	
+						}
+					}
+					else 
+					{
+						if (ae.getSource().equals(boton_home)) 
+						{	
+							try 
+							{
+								acumulador_directorios = new String[15];//REINICIAMOS
+								acumulador_directorios[0] = "/";
+
+								directorioActual = directorioInicial; //EL DIRECTORIO ACTUAL PASA A SER LA CARPETA RAÍZ
+								laber_directorio.setText(directorioActual);
+								contador=0; //REINICIAMOS EL CONTADOR DE RUTAS					
+								cliente.changeWorkingDirectory(directorioInicial);//SE ESTABLECE EL DIRECTORIO ACTUAL DE TRABAJO
+								//OBTIENE LOS FICHEROS Y ARCHIVOS DEL DIRECTORIO
+								files = cliente.listFiles();
+								llenarLista(files);//ACTUALIZA LA LISTA
+								//---------------------------------------------
+							} catch (Exception e) 
+							{
+								System.out.println(e.getMessage());
+							}
+						}
 					}
 				}
 			}
